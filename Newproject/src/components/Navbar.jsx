dfe29a09ad6eeda4c30c993f9df1539267c3bb27
OCBROW2894 <ochiengbryianowuor2894@gmail.com>
2025-06-22 23:29:18 +0300

    Touched the NavBar

--- Newproject/src/components/Navbar.jsx
+++ Newproject/src/components/Navbar.jsx
@@ -16,8 +16,15 @@ const Navbar = () => {
             window.scrollTo(0, 0); // scroll to top when logo is clicked
           }}>
             <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-            <p className="text-white text-[18px] font-bold cursor-pointer">Bryian <span className="sm:block hidden">| Owuor Ochieng</span></p>
+            <p className="text-white text-[18px] font-bold cursor-pointer">Bryian Owuor Ochieng <span className="sm:block hidden"> Software Engineer | 🎨Artist🧑🏾‍🎨</span></p>
         </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li>
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
       </div>
     </nav>
   )
